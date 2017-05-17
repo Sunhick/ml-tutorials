c1b9ee075e43263d99f85d0842e3cfa0820b4432
Sunil <sunhick@gmail.com>
2017-05-17 14:06:34 -0600

    analytical MLE calculations

--- statistics/dist/bernoulli.py
+++ statistics/dist/bernoulli.py
@@ -1,14 +1,45 @@
 #!/usr/bin/python3
 
 """
-probability distributions.
+Bernoulli distributions
+
+bernoulli.pmf(k) = 1-p  if k = 0
+                 = p    if k = 1
 
 """
 
 import sys
 
+import numpy as np
+import scipy as sp
+from scipy.stats import bernoulli
+
+
 def main(args):
-    pass
+    p = .36
+    dist = bernoulli(p)
+
+    r = np.array([0,1,1,0])
+    print("prob dist =", dist.pmf(r))
+    print("Mean =", dist.mean())
+    print("Var =", dist.var())
+    print("Std =", dist.std())
+    
+    n = 10000000
+    # generate n random numbers using bernoulli dist.
+    # r = bernoulli.rvs(p, size=1000)
+    sample = dist.rvs(size = n)
+
+    # since all numbers in r are either 0 or 1. just sum them to count the 
+    # number of ones.
+    pe = sum(sample)
+    print("p(estimate) = {0} p(actual) = {1}".format(pe/len(sample), p))
+
+    # fit a bernoulli dist.
+    sample = np.random.randint(2, size = 1000)
+    pe = sum(sample)/len(sample)
+    print("p(est) =", pe)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- statistics/dist/binom.py
+++ statistics/dist/binom.py
@@ -7,8 +7,26 @@ probability distributions.
 
 import sys
 
+import numpy as np
+import scipy as sp
+from scipy.stats import binom
+
 def main(args):
-    pass
+    n = 17
+    dist = binom(n, .765)
+
+    print("Mean =", dist.mean())
+    print("Var =", dist.var())
+    print("Std =", dist.std())
+
+    x = np.array([0,1,2,3,4,5,6])
+    print("prob dist =", dist.pmf(x))
+
+    samples = dist.rvs(size = 10000000)
+    print(set(samples), n)
+    m = len(samples)
+    p_est = sum(samples)/(m*n)
+    print("p(est) = {0} n = {1}".format(p_est, n))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
